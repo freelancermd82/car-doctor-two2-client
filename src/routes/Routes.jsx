725f04fcb6d9defcaecac9d86aa8883a205a9595
Your Name <freelancermd82@gmail.com>
2024-09-21 07:12:06 +0600

    provider

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from './../layout/Main';
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/Login/SignUp";
 
 
 const router = createBrowserRouter([
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
             path: '/',
             element: <Home></Home>
         },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/signUp',
+          element: <SignUp></SignUp>
+        }
       ]
     },
   ]);
